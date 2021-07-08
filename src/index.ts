6ee289342b1f0e45f5c72d1f60118d98f82963ef
Kevin Leguiza Gaggero <nicolaskevinleguiza@gmail.com>
2021-07-07 22:06:25 -0300

    Agregar dependencia 'ampqlib' y '@types/ampqlib'

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,15 @@
 import Discord from 'discord.js'
 import Bot from './lib/bot'
 import dotev from 'dotenv'
-// import { commandMessage } from './types/command'
 
 dotev.config()
 
-const client = new Discord.Client()
+const client: Discord.Client = new Discord.Client()
 
 client.once('ready', () => {
   console.log('Ready!')
+  // const channel101: any = client.channels.cache.find(channel => channel.id === '862138521018826782')
 })
-
 
 const bot = new Bot(client)
 
